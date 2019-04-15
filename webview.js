bf11739299d685411778e5956492f694adf96a91
Andrey Kozhuhov <kozhuhov.andrey@gmail.com>
2019-04-15 19:28:43 +0600

    Unread count

--- webview.js
+++ webview.js
@@ -1,8 +1,8 @@
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
-          const element = document.querySelector('[class="unread-count"]');
-          if(element) count = parseInt(element.innerText, 10);
+    const element = document.querySelector('[class="unread-toggle"]').innerText.match(/\d+/g);
+    if (element) count = parseInt(element, 10);
     // set Franz badge
     Franz.setBadge(count);
   };
