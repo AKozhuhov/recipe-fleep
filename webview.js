2a517437060ac1b1c62f20c5dfbbbabea75b5501
Andrey Kozhuhov <kozhuhov.andrey@gmail.com>
2019-04-16 09:56:42 +0600

    Query Selector check

--- webview.js
+++ webview.js
@@ -1,8 +1,9 @@
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
-    const element = document.querySelector('[class="unread-toggle"]').innerText.match(/\d+/g);
-    if (element) count = parseInt(element, 10);
+    const element = document.querySelector('[class="unread-toggle"]');
+    if (element)
+      if (element.innerText.match(/\d+/g))count = parseInt(element, 10);
     // set Franz badge
     Franz.setBadge(count);
   };
